a3c804cedf2125f9926fe62f69fb67006483262d
Anju Chawla <anju.chawla@georgiancollege.ca>
2016-11-30 09:17:38 -0500

    Connect to database

--- DatabaseExample/DataGridViewForm.cs
+++ DatabaseExample/DataGridViewForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace DatabaseExample
 {
-    public partial class Form1 : Form
+    public partial class DataGridViewForm : Form
     {
-        public Form1()
+        public DataGridViewForm()
         {
             InitializeComponent();
         }
